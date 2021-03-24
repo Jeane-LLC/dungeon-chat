79c83418d42d6439fb3c3b3faf3b7d18bd9f9b0c
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-24 17:04:44 -0400

    join a dungeon

---
+++ components/JoinableDungeonsTable.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TableFooter from "@material-ui/core/TableFooter";
+import TableHead from "@material-ui/core/TableHead";
+function JoinableDungeonsTable(props){
+
+    return(
+        <React.Fragment>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+              <TableRow>
+                <TableCell>Dungeon Name</TableCell>
+                <TableCell># of Occupants</TableCell>
+              </TableRow>
+              </TableHead>
+              <TableFooter>
+                
+              </TableFooter>
+            </Table>
+          </TableContainer>
+        </React.Fragment>
+    )
+}
+
+export default JoinableDungeonsTable
--- pages/join.tsx
+++ pages/join.tsx
@@ -2,13 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import JoinableDungeonsTable from "../components/JoinableDungeonsTable"
 
 function JoinPage(){
     return <div>
-    <Grid container>
+             <Grid container direction="column" spacing={1} alignItems="center">
       <Grid item>
-        <Typography>Join</Typography>
+        <Typography variant="h1">Join dungeons</Typography>
       </Grid>
+               <Grid item>
+                 <JoinableDungeonsTable/>
+               </Grid>
     </Grid>
     </div>
 }
