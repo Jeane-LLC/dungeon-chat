73d405630c5db161fc29e8313f9d9c04e3afa304
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-28 00:31:49 -0400

    new import statement

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -11,8 +11,8 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-
-import PropTypes, {InferProps} from "prop-types";
+import * as PropTypes from "prop-types";
+import {InferProps} from "prop-types";
 
 function CardField({onChange}: InferProps<typeof CardField.propTypes>){
 return(
