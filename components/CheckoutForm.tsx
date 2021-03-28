6c94c5f9eb8206570ea81ca5215969c1f29f53ae
Arjun Iyer <arjun.iyer@jeane.llc>
2021-03-28 01:17:10 -0400

    React.ComponentType

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -36,7 +36,7 @@ function Field({
   autoComplete,
   value,
   onChange,
-}: InferProps<typeof Field.propTypes>): typeof(TextField){
+}: InferProps<typeof Field.propTypes>): React.ComponentType{
     return (
     <TextField onSubmit={onSubmit} style={{"width":"100%"}} label={label} onChange={onChange} value={value} id={id} placeholder={placeholder} required={required} autoComplete={autoComplete} type={type}>
   </TextField>
